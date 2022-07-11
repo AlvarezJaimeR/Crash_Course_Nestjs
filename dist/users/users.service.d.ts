44c3765bdd728bfa69a72bafe6f43ffc314e1a68
Jaime Alvarez <jrralvarez@gmail.com>
2022-07-11 14:36:29 -0400

    Remove dist Add swagger api tag for users

--- dist/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './entities/user.entity';
-export declare class UsersService {
-    private users;
-    findAll(): User[];
-    findById(userId: number): User;
-    createUser(createUserDto: CreateUserDto): User;
-}
